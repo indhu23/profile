7312b151893be1040dc66bbc60049e83a0ceac5d
indhuja <indhuja.balakrishnan@student.fh-kiel.de>
2018-05-14 17:42:32 +0200

    adding views template

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MainSelectionView from './components/main-selection-view/main-selection-view';
 import About from './components/about/about';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MainSelectionView/>
-        <About/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedView: null
+        };
+        this.onChangeNavigation = this.onChangeNavigation.bind(this);
+    }
+
+    onChangeNavigation(value) {
+        this.setState({
+            selectedView: value
+        })
+    }
+
+    render() {
+        const {selectedView} = this.state;
+        return (
+            <div className="App">
+                <MainSelectionView onChangeNavigation={this.onChangeNavigation}/>
+                <About selectedView={selectedView}/>
+            </div>
+        );
+    }
 }
 
 export default App;
